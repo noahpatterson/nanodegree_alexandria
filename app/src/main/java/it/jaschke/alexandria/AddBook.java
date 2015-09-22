9685421322ff5e8a27be873769678fbd5ea46431
Noah Patterson <n@noahpatterson.me>
2015-09-22 16:48:44 -0400

    Add Book: Close soft keyboard when book is found

--- app/src/main/java/it/jaschke/alexandria/AddBook.java
+++ app/src/main/java/it/jaschke/alexandria/AddBook.java
@@ -1,6 +1,7 @@
 package it.jaschke.alexandria;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.database.Cursor;
 import android.os.Bundle;
@@ -14,6 +15,7 @@ import android.util.Patterns;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -93,6 +95,11 @@ public class AddBook extends Fragment implements LoaderManager.LoaderCallbacks<C
                 getActivity().startService(bookIntent);
                 AddBook.this.restartLoader();
                 mFoundBookEan = ean;
+
+                // closes the soft keyboard
+                rootView.clearFocus();
+                InputMethodManager in = (InputMethodManager)getActivity().getSystemService(Context.INPUT_METHOD_SERVICE);
+                in.hideSoftInputFromWindow(rootView.getWindowToken(), 0);
             }
         });
 
